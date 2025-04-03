c5cec2f3e7cc86ab7c44a3b2ddf59108bb81b1d6
osny.neto <osnynt@gmail.com>
2025-04-03 12:01:03 -0300

    alteração version do setup

--- osny_imagem_processing/setup.py
+++ osny_imagem_processing/setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="osny_imagem_processing",
-    version="0.0.2",
+    version="0.0.1",
     author="Osny",
     author_email="osnynt@gmail.com",
     description="Processamento de imagens com Python",
